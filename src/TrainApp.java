ab16919f1d0094406806a61ed2515518eaf1a6a0
Piyush Agarwal <agarwallapiyush27@gmail.com>
2026-03-26 15:25:12 +0530

    Update TrainApp.java

--- src/TrainApp.java
+++ src/TrainApp.java
@@ -1,7 +1,7 @@
 import java.util.regex.*;
 import java.util.Scanner;
 
-public class Main {
+public class TrainApp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
